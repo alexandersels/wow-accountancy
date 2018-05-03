b89cfa488437001c289cbeaf0e5196fc509215ce
Alexander <alexandersels@hotmail.com>
2018-05-04 00:34:15 +0200

    KKK css

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,17 +6,11 @@ import { IncomeService } from './services/income.service';
 import { AvailableGoldService } from './services/available-gold.service';
 import { TeamService } from './services/team.service';
 import { SplitService } from './services/split.service';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule, MatIconModule, MatListModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
 import {ReactiveFormsModule} from "@angular/forms";
 
 
 const MODULES = [
-  MatSidenavModule,
-  MatListModule,
-  MatIconModule,
-  MatButtonModule,
   MatInputModule,
   ReactiveFormsModule
 ];
